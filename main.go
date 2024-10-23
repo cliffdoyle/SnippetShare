d48c0d8e5540688a79337fec23e9f14d7ba797b9
clomollo <oyoocliff471@gmail.com>
2024-10-23 23:50:18 +0300

    Add restricted /snippet/create route to act on POST requests only

--- main.go
+++ main.go
@@ -6,22 +6,31 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path !="/"{
+		http.NotFound(w,r)
+		return
+	}
 	w.Write([]byte(`hello from SnippetShare`))
 }
-//This handle will handle the viewing of snippets
+//This handler will handle the viewing of snippets
 func snipview(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Display a specific code snippet"))
 }
 
 func createsnip(w http.ResponseWriter, r *http.Request){
+	if r.Method !=http.MethodPost{
+		w.WriteHeader(405)
+		w.Write([]byte("Method not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new code snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, home)
-	mux.HandleFunc("/snipview", snipview)
-	mux.HandleFunc("/createsnip", createsnip)
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/snipview", snipview)
+	mux.HandleFunc("/snippet/createsnip", createsnip)
 	// fmt.Println(`Hello world`)
 
 	log.Print("starting server on :4040")

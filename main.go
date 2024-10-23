7c38564b47d18bf0376c1cfd572a5ea843d60c26
clomollo <oyoocliff471@gmail.com>
2024-10-23 21:28:24 +0300

    Add two routes and created handlers to handle request from them

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`hello from SnippetShare`))
 }
 
+func snipview(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific code snippet"))
+}
+
+func createsnip(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create a new code snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc(`/`, home)
+	mux.HandleFunc("/snipview", snipview)
+	mux.HandleFunc("/createsnip", createsnip)
 	// fmt.Println(`Hello world`)
 
 	log.Print("starting server on :4040")
